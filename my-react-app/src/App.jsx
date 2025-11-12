cda8337e0fc3334a5a19623d27fcd39e25bbead3
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-11-12 10:59:10 -0800

    home-page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Componet/header/Header'
+import Home from './Componet/home/Home'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 // import 'bootstrap-icons/font/bootstrap-icons.css'
 function App() {
   return (
     <div>
-      <Header/>
+      {/* <Header/> */}
+      <Home/>
       
     </div>
   )
