bf592785328281b2b9b494facec4cae2b4973551
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2025-11-12 12:16:39 -0800

    Time-less-product

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Componet/header/Header'
 import Home from './Componet/home/Home'
+import Timelessproduct from './Componet/Timeless/Timelessproduct'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 // import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -9,6 +10,7 @@ function App() {
     <div>
       {/* <Header/> */}
       <Home/>
+      <Timelessproduct/>
       
     </div>
   )
---
+++ my-react-app/src/Componet/Timeless/Timelessproduct.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import leftfront from '../../assets/image/1731949033.jpg'
+import './timeless.css'
+function Timelessproduct() {
+  return (
+    <div>
+        <div className="internal-container">
+            <div className="left-container">
+                <div className="left-all">
+                    <div className="left-back">
+                        {/* <img src="" alt="" /> */}
+                        <div className="left-front-img">
+                            <img src={leftfront} alt="" />
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className="right-container">
+                <div className="right-internal">
+                    <div className="riht-title">
+                        <h2>Timeless Furniture,Natural <br /> Beauty</h2>
+                    </div>
+                    <div className="descrption">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />
+                             Perferendis nesciunt fugit nobis nulla similique totam cum dolorem. <br />
+                              Delectus illo distinctio est nesciunt consequuntur, <br />
+                             doloremque totam? Vitae incidunt magnam qui voluptate.</p>
+                    </div>
+                    <div className="timeless-note">
+                        <div className="first-row">
+                            <div className="timeless">
+                                 <p>Time less Dessign</p>
+                            </div>
+                            <div className="timeless">
+                                 <p>Time less Dessign</p>
+                            </div>
+                            <div className="timeless">
+                                 <p>Time less Dessign</p>
+                            </div>
+                        </div>
+                         <div className="second-row">
+                          <div className="timeless">
+                                 <p>Time less Dessign</p>
+                            </div>
+                            
+                        </div>
+                      
+
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Timelessproduct
